79d70c3f06740699b3f6c25e62fdc3a7bfd65cdf
Samuel Weibel <samuel.weibel@sea.ch>
2019-09-19 14:29:51 +0200

    Added special addresses to dnsmasq configuration to fix the portal on Android.
    Change-type: patch
    Signed-off-by: Samuel Weibel <samuel.weibel@sea.ch>

--- src/dnsmasq.rs
+++ src/dnsmasq.rs
@@ -7,6 +7,16 @@ use config::Config;
 
 pub fn start_dnsmasq(config: &Config, device: &Device) -> Result<Child> {
     let args = [
+        // the following addresses were taken from 
+        // https://github.com/tretos53/Captive-Portal/blob/master/dnsmasq.conf
+        // they are required because otherwise the WiFi portal will not 
+        // be shown automatically when connecting to the WiFi on Android devices
+        "--address=/connectivitycheck.gstatic.com/216.58.206.131",
+        "--address=/www.gstatic.com/216.58.206.99",
+        "--address=/www.apple.com/2.16.21.112",
+        "--address=/captive.apple.com/17.253.35.204",
+        "--address=/clients3.google.com/216.58.204.46",
+        "--address=/www.msftconnecttest.com/13.107.4.52",
         &format!("--address=/#/{}", config.gateway),
         &format!("--dhcp-range={}", config.dhcp_range),
         &format!("--dhcp-option=option:router,{}", config.gateway),
